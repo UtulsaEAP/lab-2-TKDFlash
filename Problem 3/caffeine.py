82e044a7952db5ddd48cff9960aaadcfd62ad1ec
TKDFlash <113069942+TKDFlash@users.noreply.github.com>
2024-02-05 15:03:40 -0600

    mommy

--- Problem 3/caffeine.py
+++ Problem 3/caffeine.py
@@ -1,7 +1,14 @@
 
 def caffeine():
-    caffeine_mg = float(input())
+#    caffeine_mg = float(input())
     ''' Type your code here. '''
+
+    caffeine_intake = float(input())
+    hr_6 = caffeine_intake*(0.5**(6/6))
+    hr_12 = caffeine_intake*(0.5**(12/6))
+    hr_24 = caffeine_intake*(0.5**(24/6))
+
+    print(f'After 6 hours: {hr_6:.2f} mg\n'f'After 12 hours: {hr_12:.2f} mg\n'f'After 24 hours: {hr_24:.2f} mg')
     
 if __name__ == "__main__":
     caffeine()
