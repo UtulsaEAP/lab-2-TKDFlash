023729b077e3332003c632b15ded627ccd8184fc
TKDFlash <113069942+TKDFlash@users.noreply.github.com>
2024-02-05 14:03:13 -0600

    commy

--- Problem 1/right_arrow.py
+++ Problem 1/right_arrow.py
@@ -2,8 +2,12 @@ def right_arrow():
     base_char = input()
     head_char = input()
 
-    row1 = '      ' + head_char
+#    row1 = '      ' + head_char
     ''' Type your code here. '''
+
+    row1 = '      ' + head_char
+    row2 = base_char + base_char + base_char + base_char + base_char + base_char + head_char + head_char
+    row3 = base_char + base_char + base_char + base_char + base_char + base_char + head_char + head_char + head_char
 
     print(row1)
     print(row2)
