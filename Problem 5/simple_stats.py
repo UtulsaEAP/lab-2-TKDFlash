b43bcc42b44a7cfbe3a97e4745da4d67f06cda70
TKDFlash <113069942+TKDFlash@users.noreply.github.com>
2024-02-05 21:39:40 -0600

    J.A.R.V.I.S.

--- Problem 5/simple_stats.py
+++ Problem 5/simple_stats.py
@@ -5,5 +5,15 @@ def simple_stats():
     num3 = float(input())
     num4 = float(input())
     ''' Type your code here. '''
+
+    product_1 = num1*num2*num3*num4
+    average_1 = (num1+num2+num3+num4)/4
+
+    product_2 = num1*num2*num3*num4
+    average_2 = (num1+num2+num3+num4)/4
+
+    print(f'{product_1:.0f}',f'{average_1:.0f}')
+    print(f'{product_2:.3f}',f'{average_2:.3f}')
+
 if __name__ == "__main__":
     simple_stats()
